24a3bf6080061d0972c53450900e950d5f31f904
Joshua Rogers <github@joshuarogers.net>
2011-05-23 23:18:52 -0500

    Added example plugins.

---
+++ lib/plugin/kick.rb
@@ -0,0 +1,10 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+require 'plugin'
+
+class Kick < Plugin
+  def initialize
+    
+  end
+end
---
+++ lib/plugin/plus_plus.rb
@@ -0,0 +1,10 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+require 'plugin'
+
+class Plus_Plus < Plugin
+  def initialize
+    
+  end
+end
