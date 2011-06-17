53e810584bfda2841c125079031f33d1500d8389
Joshua Rogers <github@joshuarogers.net>
2011-06-16 22:55:56 -0500

    Added echo.

---
+++ lib/plugin/echo.rb
@@ -0,0 +1,14 @@
+require 'plugin'
+
+class Echo < Plugin
+  def send(message)
+    regex = /^[\/|\\]echo\s/i
+    if (message.text =~ regex)
+      message.text = message.text.gsub(regex, "")
+      message.format = false
+      message.return_to_sender
+    end
+
+    message
+  end
+end
