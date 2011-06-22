9bbe32684a66a27a9cf98a71856c7df0da79f0eb
Joshua Rogers <github@joshuarogers.net>
2011-06-21 19:40:57 -0500

    Complete the busy plugin.

--- lib/plugin/busy.rb
+++ lib/plugin/busy.rb
@@ -2,7 +2,14 @@
 # and open the template in the editor.
 
 class Busy < Plugin
-  def initialize
+  
+  def send(message)
+    message.reciptients = message.reciptients.select { |person| !person.busy? }
     
+    if message.sender.busy?
+      message.return_to_sender "You cannot chat while 'busy.'"
+    end
+    
+    message
   end
 end
